ba2536fcd9f6a26d6217311e9e3ff79192c98ea3
reutki <marcu.reutki@gmail.com>
2023-03-21 10:45:34 +0200

    added redux to the app, configured Provided and added store

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,13 +1,16 @@
 // pages/_app.js
 
 // 1. import `NextUIProvider` component
+import store from "@/store";
 import { NextUIProvider } from "@nextui-org/react";
-
+import { Provider } from "react-redux";
 function MyApp({ Component, pageProps }) {
   // 2. Use at the root of your app
   return (
     <NextUIProvider>
-      <Component {...pageProps} />
+      <Provider store={store}>
+        <Component {...pageProps} />
+      </Provider>
     </NextUIProvider>
   );
 }
---
+++ src/store.js
@@ -0,0 +1,6 @@
+import { configureStore } from "@reduxjs/toolkit";
+
+//here we have stored all the api's created from redux.
+export default configureStore({
+  reducer: {},
+});
